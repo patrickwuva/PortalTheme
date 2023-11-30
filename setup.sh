d5f44fc78919aead1cccd368d4fe904650cd76d4
patrickwuva <91228237+patrickwuva@users.noreply.github.com>
2023-11-30 18:32:11 -0500

    Updating setup.sh
    Updating setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 !/bin/bash
-mv $HOME/.bashrc $HOME/.bashrc.bkp; wget -O $HOME/.bashrc https://raw.githubusercontent.com/patrickwuva/PortalTheme/main/bashrc; . $HOME/.bashrc
+cp $HOME/.bashrc $HOME/.bashrc.bkp; wget -O $HOME/.bashrc https://raw.githubusercontent.com/patrickwuva/PortalTheme/main/bashrc; . $HOME/.bashrc
